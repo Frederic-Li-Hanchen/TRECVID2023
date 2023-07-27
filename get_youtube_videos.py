6324f520e1cca22c86f917227e7e540ef017d34a
Frederic-Li-Hanchen <frederic.li.hanchen@gmail.com>
2023-07-27 10:12:12 +0200

    added verbose and login option

--- get_youtube_videos.py
+++ get_youtube_videos.py
@@ -10,9 +10,10 @@ from pdb import set_trace as st
 ### [str] url: Youtube url
 ### [str] save_path: path to the folder where the video should be saved
 ### [str] file_name: name of the file to be saved, without extension
-def download_video(url,save_path,file_name):
+### [bool] login: use authentification information to obtain videos behind age verification
+def download_video(url,save_path,file_name,login=False):
     # Get the video
-    yt = YouTube(url)
+    yt = YouTube(url,use_oauth=login,allow_oauth_cache=login)
     # List streams per quality
     stream_list = yt.streams.filter(adaptive=True)
     # Get tag of the first stream in the list
@@ -30,32 +31,45 @@ def download_video(url,save_path,file_name):
 ### Function to download all videos given a list of videos in csv format
 ### [str] csv_file: path to the csv file containing url, sample IDs and start/end timestamps
 ### [str] save_path: path to the folder where videos should be saved
-def download_all_videos(csv_file,save_path):
+### [int list] sample_list: list of samples to process (optional)
+### [bool] login: use authentification information to obtain videos behind age verification (default: False)
+### [bool] verbose: shows the progress for the clipping of the videos (default: False)
+def download_all_videos(csv_file,save_path,sample_list=[],login=False,verbose=False):
     # Prepare save folder
     if not os.path.exists(save_path):
         os.makedirs(save_path)
     # Read the csv file of examples
-    file_list = pd.read_csv(csv_file)
+    file_list_all = pd.read_csv(csv_file)
+    # Filter the list of samples to keep only the selected ones
+    if len(sample_list) > 0:
+        file_list = file_list_all[file_list_all['sample_id'].isin(sample_list)]
+    else:
+        file_list = file_list_all
     nb_examples = len(file_list)
     # Loop on the videos
+    if verbose:
+        log = 'bar'
+    else:
+        log = None
     for idx in range(nb_examples):
     #for idx in range(5): # DEBUG
         url = file_list.iloc[idx]['video_url']
         file_name = file_list.iloc[idx]['sample_id']
         start_ts = file_list.iloc[idx]['answer_start_second']
         end_ts = file_list.iloc[idx]['answer_end_second']
+        start = time()
         try:
-            start = time()
-            video_path, video_name = download_video(url,save_path,str(file_name)+'_tmp')
-        except:
+            video_path, video_name = download_video(url,save_path,str(file_name)+'_tmp',login)
+        except Exception as error:
             print('ERROR: video download failed for sample %d!'%file_name)
+            print(error)
         else:
             # Extract clip
             clip = VideoFileClip(os.path.join(video_path,video_name)).subclip(start_ts,end_ts)
             # Prepare name of video to save
             new_video_name = video_name.replace('_tmp','')
             # Save video clip
-            clip.write_videofile(os.path.join(video_path,new_video_name),logger=None)
+            clip.write_videofile(os.path.join(video_path,new_video_name),logger=log)
             # Delete old video
             clip.close()
             os.remove(os.path.join(video_path,video_name))
@@ -70,6 +84,6 @@ if __name__ == '__main__':
     # download_video('https://www.youtube.com/watch?v=h5MvX50zTLM','./results','video')
 
     # Download all videos
-    download_all_videos('./results/val_set.csv','./results/val_videos')
-    #download_all_videos('./results/train_set.csv','./results/train_videos')
-    #download_all_videos('./results/test_set.csv','./results/test_videos')
+    #download_all_videos('./results/val_set.csv','./results/val_videos')
+    download_all_videos('./results/train_set.csv','./results/train_videos',[1068],verbose=True)
+    #download_all_videos('./results/test_set.csv','./results/test_videos',[2718,2719,2720,2721,2722],login=True)
