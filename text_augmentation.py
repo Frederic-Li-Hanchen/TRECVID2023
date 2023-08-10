354b4d5000373e2e918b81926f60ef1d604027c7
Frederic-Li-Hanchen <frederic.li.hanchen@gmail.com>
2023-08-10 10:44:30 +0200

    fixed a small bug with sample IDs

--- text_augmentation.py
+++ text_augmentation.py
@@ -1,10 +1,10 @@
 import nltk
 from pdb import set_trace as st
 import pandas as pd
-# nltk.download('punkt')
-# nltk.download('wordnet')
-# nltk.download('stopwords')
-# nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
 from textaugment import EDA, Word2vec, Wordnet, Translate
 import gensim
 
@@ -13,8 +13,8 @@ import gensim
 ### [str] csv_path: path to the csv file containing the dataset
 ### [str] res_path: path to the csv file of results to be saved
 ### [int] nb_sentences: nb of sentences to be generated
-### [str] augmentation: type of approach for augmentation. Has to be in ['eda','word2vec','wordnet','translation','double_translation']
-def dataset_augmentation(csv_path, res_path, nb_sentences=3, augmentation='eda'):
+### [str] augmentation: type of approach for augmentation. Has to be in ['eda','word2vec','wordnet','translation','double_translation','hybrid']
+def dataset_augmentation(csv_path, res_path, nb_sentences=2, augmentation='eda'):
 
     # Load the dataset
     data = pd.read_csv(csv_path)
@@ -33,9 +33,12 @@ def dataset_augmentation(csv_path, res_path, nb_sentences=3, augmentation='eda')
     elif augmentation == 'double_translation':
         augmenter = Translate(src='en',to='fr')
         augmenter2 = Translate(src='en',to='de')
+    elif augmentation == 'hybrid':
+        augmenter = Wordnet()
+        augmenter2 = EDA()
     else:
         print('ERROR: improper augmentation technique! Must take one of the following values:')
-        print(['eda','word2vec','wordnet','translation','double_translation'])
+        print(['eda','word2vec','wordnet','translation','double_translation','hybrid'])
         return
 
     running_idx = 0
@@ -58,25 +61,51 @@ def dataset_augmentation(csv_path, res_path, nb_sentences=3, augmentation='eda')
         # Get the question
         question = data['question'].iloc[idx]
         # Perform augmentation on the question as many times as requested
-        for augment_idx in range(nb_sentences):
-            if augmentation == 'eda':
-                new_sentence = augmenter.synonym_replacement(question)
-            elif augmentation == 'double_translation':
-                tmp_new_sentence = augmenter.augment(question)
-                new_sentence = augmenter2.augment(tmp_new_sentence)
-            else:
+        if augmentation == 'hybrid':
+            for augment_idx in range(nb_sentences):
                 new_sentence = augmenter.augment(question)
-            res.loc[running_idx] = [tmp_sample_id+0.1*(augment_idx+1),
-                        new_sentence,
-                        tmp_answer_start,
-                        tmp_answer_end,
-                        tmp_answer_start_second,
-                        tmp_answer_end_second,
-                        tmp_video_length,
-                        tmp_video_id,
-                        tmp_video_url,
-                        tmp_transcript]
-            running_idx += 1
+                new_sentence2 = augmenter2.synonym_replacement(question)
+                res.loc[running_idx] = [tmp_sample_id+0.1*(augment_idx+1),
+                            new_sentence,
+                            tmp_answer_start,
+                            tmp_answer_end,
+                            tmp_answer_start_second,
+                            tmp_answer_end_second,
+                            tmp_video_length,
+                            tmp_video_id,
+                            tmp_video_url,
+                            tmp_transcript]
+                res.loc[running_idx+1] = [tmp_sample_id+0.1+0.1*(augment_idx+2),
+                            new_sentence2,
+                            tmp_answer_start,
+                            tmp_answer_end,
+                            tmp_answer_start_second,
+                            tmp_answer_end_second,
+                            tmp_video_length,
+                            tmp_video_id,
+                            tmp_video_url,
+                            tmp_transcript]
+                running_idx += 2
+        else:
+            for augment_idx in range(nb_sentences):
+                if augmentation == 'eda':
+                    new_sentence = augmenter.synonym_replacement(question)
+                elif augmentation == 'double_translation':
+                    tmp_new_sentence = augmenter.augment(question)
+                    new_sentence = augmenter2.augment(tmp_new_sentence)
+                else:
+                    new_sentence = augmenter.augment(question)
+                res.loc[running_idx] = [tmp_sample_id+0.1*(augment_idx+1),
+                            new_sentence,
+                            tmp_answer_start,
+                            tmp_answer_end,
+                            tmp_answer_start_second,
+                            tmp_answer_end_second,
+                            tmp_video_length,
+                            tmp_video_id,
+                            tmp_video_url,
+                            tmp_transcript]
+                running_idx += 1
     
     # Save the new result frame
     res.to_csv(res_path,index=False)
@@ -97,5 +126,7 @@ if __name__ == '__main__':
     # dataset_augmentation(csv_path='./results/val_set.csv', res_path='./results/augmented_val_set_translate.csv', nb_sentences=3, augmentation='translation')
 
     # Double translate augmentation
-    dataset_augmentation(csv_path='./results/val_set.csv', res_path='./results/augmented_val_set_double_translate.csv', nb_sentences=3, augmentation='double_translation')
+    #dataset_augmentation(csv_path='./results/val_set.csv', res_path='./results/augmented_val_set_double_translate.csv', nb_sentences=3, augmentation='double_translation')
 
+    # Augmentation with both WordNet and Synonyms
+    dataset_augmentation(csv_path='./data/train_set.csv', res_path='./results/augmented_train_set_hybrid.csv', nb_sentences=2, augmentation='hybrid')
